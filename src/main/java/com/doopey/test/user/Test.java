ec9172ce9b3931b6458e4c00c090296f13510110
fangchen <fangchen@xiaomi.com>
2016-12-27 17:53:36 +0800

    reset 3

--- src/main/java/com/doopey/test/user/Test.java
+++ src/main/java/com/doopey/test/user/Test.java
@@ -18,7 +18,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println(test());
-        // 
+        //
+        // 2
+        // 3
     }
 
     public static int test() {
